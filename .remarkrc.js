95c492a2e5b75da49f68043a1e00e7e5d7e27ce8
Luke Oliff <luke@lukeoliff.com>
2020-11-14 10:30:49 +0000

    feat: configure remark and release

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,17 +1,5 @@
 exports.plugins = [
   require('remark-frontmatter'),
-  [
-    require('remark-retext'),
-    require('unified')().use({
-      plugins: [
-        require('retext-english'),
-        require('retext-syntax-urls'),
-        [require('retext-sentence-spacing'), { preferred: 1 }],
-        require('retext-repeated-words'),
-        require('retext-usage'),
-      ],
-    }),
-  ],
   require('remark-preset-lint-consistent'),
   require('remark-preset-lint-recommended'),
   require('remark-preset-lint-markdown-style-guide'),
